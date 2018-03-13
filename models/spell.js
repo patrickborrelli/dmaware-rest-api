6bdc80a8eb31cf77763a57743c8b3b3aec4aaec2
Patrick Borrelli <patrickborrelli@gmail.com>
2018-03-12 21:06:23 -0400

    Modifications to Spell model to handle imported data

--- models/spell.js
+++ models/spell.js
@@ -3,15 +3,32 @@ var Schema = mongoose.Schema;
 
 var Spell = new Schema({
     level: Number,
-    character_class: {
+    character_class: [{
         type: String,
         enum: ['Bard', 'Cleric', 'Druid', 'Paladin', 'Ranger', 'Sorcerer', 'Warlock', 'Wizard']
-    },
+    }],
     name: {
         type: String,
         trim: true
     },
     description: {
+        type: String,
+        trim: true
+    },
+    range:  {
+        type: String,
+        trim: true
+    },
+    ritual: Boolean,
+    school:  {
+        type: String,
+        trim: true
+    },
+    duration:  {
+        type: String,
+        trim: true
+    }, 
+    casting_time:  {
         type: String,
         trim: true
     }
