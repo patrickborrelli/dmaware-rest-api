468f7a51884f5536ffc984ca6bde4c54eb2be762
Patrick Borrelli <patrickborrelli@gmail.com>
2018-04-06 14:13:45 -0400

    mongo URI as environment variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var secretKey = '12345-67890-09876-54321';
-var mongoUrl = 'mongodb://patrickborrelli:y7y54ptr@ds237389.mlab.com:37389/heroku_f959rcg9';
+var mongoUrl = process.env.MONGODB_URI;
 var fbClientId = process.env.DMA_FB_CLIENTID;
 var fbClientSecret = process.env.DMA_FB_SECRET;
 var mailApiKey = process.env.DMA_MAILGUN_KEY;
